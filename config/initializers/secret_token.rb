7746944fe217038b9e7b6638f272eaf850a01a41
John Perry <jgperry@gmail.com>
2013-08-14 11:44:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GgaRails::Application.config.secret_key_base = '2a217a86d3ea437bfc6c799907548ab46f512f621def847f5f0255a931d650a65b43fa8bc193543e0963eb2adf77b7bb0e024c9e4a8267976b8f18c76ea66fe9'
